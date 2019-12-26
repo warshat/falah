439dcfacef9cdbcb80038d307a6c68589303b8e1
hasauino <oh91@windowslive.com>
2019-12-26 23:16:47 +0100

    added gender field to user

--- main/models.py
+++ main/models.py
@@ -3,6 +3,13 @@ from django.contrib.auth.models import AbstractUser
 from django.core.validators import EmailValidator
 from location_field.models.plain import PlainLocationField
 
+
+gender_choices = [
+    ('male', 'ذكر'),
+    ('female', 'أنثى')
+]
+
+
 class User(AbstractUser):
     email = models.EmailField(null=True, blank=True, unique=True,
                               validators=[EmailValidator(message="your custom message")])
@@ -10,6 +17,11 @@ class User(AbstractUser):
 
     city = models.CharField(max_length=255, null=True)
     location = PlainLocationField(based_fields=['city'], zoom=7, null=True)
+    gender = models.CharField(
+        max_length=10,
+        choices=gender_choices,
+        verbose_name = 'الجنس'
+        )
 
 
 
